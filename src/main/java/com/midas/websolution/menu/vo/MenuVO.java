ff2423f61983170dcf61e150933e5ce15d7e4c2c
HyunhooKwon <kwongusgn@gmail.com>
2017-05-27 13:48:56 +0900

    CREATE MenuVO.java

--- src/main/java/com/midas/websolution/menu/vo/MenuVO.java
+++ src/main/java/com/midas/websolution/menu/vo/MenuVO.java
@@ -2,5 +2,36 @@ package com.midas.websolution.menu.vo;
 
 
 public class MenuVO {
-
+	private int MENU_NO;
+	private String MENU_DT;
+	private int MENU_KIND;
+	private String MENU_FILE_NAME;
+	
+	
+	public int getMENU_NO() {
+		return MENU_NO;
+	}
+	public void setMENU_NO(int mENU_NO) {
+		MENU_NO = mENU_NO;
+	}
+	public String getMENU_DT() {
+		return MENU_DT;
+	}
+	public void setMENU_DT(String mENU_DT) {
+		MENU_DT = mENU_DT;
+	}
+	public int getMENU_KIND() {
+		return MENU_KIND;
+	}
+	public void setMENU_KIND(int mENU_KIND) {
+		MENU_KIND = mENU_KIND;
+	}
+	public String getMENU_FILE_NAME() {
+		return MENU_FILE_NAME;
+	}
+	public void setMENU_FILE_NAME(String mENU_FILE_NAME) {
+		MENU_FILE_NAME = mENU_FILE_NAME;
+	}
+	
+	
 }
