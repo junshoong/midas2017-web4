cf3e5e4a01374a29b1f9f57fcc9b3b95f611ab40
HyunhooKwon <kwongusgn@gmail.com>
2017-05-27 17:09:36 +0900

    MenuService - uploadFile 메소드 구현

--- src/main/java/com/midas/websolution/menu/service/MenuService.java
+++ src/main/java/com/midas/websolution/menu/service/MenuService.java
@@ -1,5 +1,7 @@
 package com.midas.websolution.menu.service;
 
+import org.springframework.web.multipart.MultipartFile;
 
 public interface MenuService {
+	public void uploadFile(MultipartFile file, String file_path);
 }
--- src/main/java/com/midas/websolution/menu/service/MenuServiceImpl.java
+++ src/main/java/com/midas/websolution/menu/service/MenuServiceImpl.java
@@ -1,5 +1,9 @@
 package com.midas.websolution.menu.service;
 
+import java.io.File;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import com.midas.websolution.menu.dao.MenuDao;
 
 public class MenuServiceImpl implements MenuService{
@@ -8,6 +12,14 @@ public class MenuServiceImpl implements MenuService{
 	
 	public void setMenuDao(MenuDao menuDao) {
 		this.menuDao = menuDao;
+	}
+
+	@Override
+	public void uploadFile(MultipartFile file, String file_path) {
+		// TODO Auto-generated method stub
+		String file_name = file.getOriginalFilename();
+		
+		File new_file = new File("");
 	}
 	
 }
